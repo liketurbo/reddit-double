b8447546c428ac4c85114d6e84e1c2cd55892aee
Ramzan Chasygov <theliketurbo@gmail.com>
2020-08-20 22:21:02 +0300

    feat: set up "MicroORM"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import { MikroORM } from "mikro-orm";
+import { __prod__ } from "./constants";
+import MikroOrmConfig from "./mikro-orm.config";
+import Post from "./entities/Post";
+
+const start = async () => {
+  const orm = await MikroORM.init(MikroOrmConfig);
+
+  await orm.getMigrator().up();
+
+  const post = orm.em.create(Post, { title: "Hello, World!" });
+  orm.em.persistAndFlush(post);
+};
+
+start().catch((err) => {
+  console.error(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import Post from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORMOptions } from "mikro-orm";
+import path from "path";
+
+const MikroOrmConfig = {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  dbName: "reddit-double",
+  type: "postgresql",
+  debug: !__prod__,
+  user: "Ramzan",
+  password: "postgres",
+  entities: [Post] as any,
+} as MikroORMOptions;
+
+export default MikroOrmConfig;
